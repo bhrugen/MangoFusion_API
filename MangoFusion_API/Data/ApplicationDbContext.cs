53aa8ed212fd5fcb89d0b66cd95da82a0893d4f2
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-24 15:10:01 -0500

    Add Name to User Table

--- MangoFusion_API/Data/ApplicationDbContext.cs
+++ MangoFusion_API/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MangoFusion_API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MangoFusion_API.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
