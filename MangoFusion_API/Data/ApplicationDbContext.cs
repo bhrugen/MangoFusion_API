719c0052933102a78a84e581ec641ec6d1b224eb
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-24 14:53:25 -0500

    Connection String and ApplicationDbContext

---
+++ MangoFusion_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangoFusion_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
