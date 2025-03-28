dcf008f66a5bb24e841bce1edc4260d5509b0b80
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-28 16:54:35 -0500

    Login Endpoint

--- MangoFusion_API/Controllers/AuthController.cs
+++ MangoFusion_API/Controllers/AuthController.cs
@@ -3,7 +3,11 @@ using MangoFusion_API.Models.Dto;
 using MangoFusion_API.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 
 namespace MangoFusion_API.Controllers
 {
@@ -86,6 +90,78 @@ namespace MangoFusion_API.Controllers
             }
             
         }
-        
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var userFromDb = await _userManager.FindByEmailAsync(model.Email);
+                if (userFromDb != null)
+                {
+                    bool isValid = await _userManager.CheckPasswordAsync(userFromDb, model.Password);
+                    if (!isValid)
+                    {
+                        _response.Result = new LoginResponseDTO();
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        _response.ErrorMessages.Add("Invalid Credentials");
+                        return BadRequest(_response);
+                    }
+                    JwtSecurityTokenHandler tokenHandler = new();
+                    byte[] key = Encoding.ASCII.GetBytes(secretKey);
+
+                    SecurityTokenDescriptor tokenDescriptor = new()
+                    {
+                        Subject = new ClaimsIdentity(
+                            [
+                            new ("fullname",userFromDb.Name),
+                            new ("id",userFromDb.Id),
+                            new (ClaimTypes.Email,userFromDb.Email!.ToString()),
+                            new (ClaimTypes.Role, _userManager.GetRolesAsync(userFromDb).Result.FirstOrDefault()!)
+                            ]),
+                        Expires = DateTime.UtcNow.AddDays(7),
+                        SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+                    LoginResponseDTO loginResponse = new()
+                    {
+                        Email = userFromDb.Email,
+                        Token = tokenHandler.WriteToken(token),
+                        Role = _userManager.GetRolesAsync(userFromDb).Result.FirstOrDefault()!
+                    };
+
+                    _response.Result = loginResponse;
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+
+                }
+                _response.Result = new LoginResponseDTO();
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Invalid Credentials");
+                return BadRequest(_response);
+
+            }
+            else
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                foreach (var error in ModelState.Values)
+                {
+                    foreach (var item in error.Errors)
+                    {
+                        _response.ErrorMessages.Add(item.ErrorMessage);
+                    }
+                }
+                return BadRequest(_response);
+            }
+
+        }
     }
 }
