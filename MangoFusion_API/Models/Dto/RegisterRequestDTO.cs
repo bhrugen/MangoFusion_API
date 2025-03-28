41af76d6596ebbdcf03435810c72e858a785eb9b
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-28 16:28:18 -0500

    Register Endpoint

--- MangoFusion_API/Models/Dto/RegisterRequestDTO.cs
+++ MangoFusion_API/Models/Dto/RegisterRequestDTO.cs
@@ -9,7 +9,7 @@ namespace MangoFusion_API.Models.Dto
         [Required]
         public string Password { get; set; } = string.Empty;
         [Required]
-        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
     }
 }
