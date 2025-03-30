6f5e802f11f531f9731fcf35e0b23a42b983bb58
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-30 14:32:20 -0500

    Add Rating Field

---
+++ MangoFusion_API/Models/Dto/OrderDetailsUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion_API.Models.Dto
+{
+    public class OrderDetailsUpdateDTO
+    {
+        [Required]
+        public int OrderDetailId { get; set; }
+        [Required]
+        public int MenuItemId { get; set; }
+        [Required]
+        public int Rating { get; set; }
+    }
+}
